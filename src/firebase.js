212a525edffad2cfa2ed16ed62ceb366f6713555
kiritox123 <kiritox123@gmail.com>
2020-04-19 00:10:59 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,10 @@ import firebase from 'firebase/app'
 require('firebase/auth')
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDON581xVOP9Wjae4rWAT1Emk2H5j0suHQ",
-    authDomain: "flight-app-57479.firebaseapp.com",
-    databaseURL: "https://flight-app-57479.firebaseio.com",
-    storageBucket: "flight-app-57479.appspot.com"
+    apiKey: "APi_firebase goes here",
+    authDomain: "authDomain-goes here",
+    databaseURL: "databaseURL -goes here",
+    storageBucket: "storageBucket - goes here"
   };
 
   firebase.initializeApp(firebaseConfig)
